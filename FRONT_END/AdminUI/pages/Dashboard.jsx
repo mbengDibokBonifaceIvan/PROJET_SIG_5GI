0433090f59ebd3d3d735e441944cf51d560eb41d
Ivan <mbengivan63@gmail.com>
2024-12-12 00:00:14 +0100

    reajustement de la structure de la branche

---
+++ FRONT_END/AdminUI/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import "./Dashboard.css";
+import Card from "../../../FRONTEND/Components/Card";
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <Card />
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ FRONT_END/components/ui/skeleton.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/FRONT_END/lib/utils";
+
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-muted", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
