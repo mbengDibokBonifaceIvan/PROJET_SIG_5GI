28d69f5775dd38a5d0df66fe553a9c104c17e330
Ivan <mbengivan63@gmail.com>
2024-12-14 09:33:55 +0100

    Gestion du 1er histogramme terminee

--- FRONT_END/app/Components/Histogramme/Histogramme.tsx
+++ FRONT_END/app/Components/Histogramme/Histogramme.tsx
@@ -1,139 +1,105 @@
-import React from "react";
-import {
-  useGlobalContext,
-  useGlobalContextUpdate,
-} from "@/app/context/globalContext";
+import React, { useEffect, useState } from "react";
+import { useGlobalContext } from "@/app/context/globalContext";
 import { Skeleton } from "@/components/ui/skeleton";
 import { pollIcon } from "../../utils/Icons";
+//import { Candidat, Resultat } from "../lib/utils"; // Might not be needed depending on your data structure
+import axios from "axios";
 
 const colors = ["#FF6633", "#FFB399", "#FF33FF", "#3366E6", "#00a64f"];
 
-interface CandidateData {
-  main: { temp_min: number; temp_max: number };
-  dt: number;
-}
-
-interface ProcessedData {
-  candidate: string;
-  votes: number;
-}
-
-interface Region {
-  id_region: number;
-  nom_region: string;
-}
-
-interface Departement {
-  id_departement: number;
-  nom_departement: string;
-  region: Region;
-}
-
-interface Arrondissement {
-  id_arrondissement: number;
-  nom_arrondissement: string;
-  departement: Departement;
-}
-
-interface CentreDeVote {
-  id_centre_vote: number;
-  nom_centre: string;
-  arrondissement: Arrondissement;
-}
-
-interface Coordonnees {
-  latitude: number;
-  longitude: number;
-}
-
-interface Candidat {
-  id_candidat: number;
-  nom_candidat: string;
-  parti_politique: string;
-}
-
-interface BureauDeVote {
-  id_bureau_vote: number;
-  nom_bureau: string;
-  coordonnees: Coordonnees;
-  centreVote: CentreDeVote;
-}
-
-interface Resultats {
-  id_resultat: number;
-  bureauVote: BureauDeVote;
-  candidat: Candidat;
-  nombre_voix: number;
-  date_saisie: Date;
-  annee_election: number;
-}
-
-function processData(
-  votesResults: Resultats[],
-  candidatData: Candidat[]
-): ProcessedData[] {
-  return candidatData.map((candidat, index) => {
-    const totalVotes = votesResults
-      .filter((result) => result.candidat.id_candidat === candidat.id_candidat)
-      .reduce((acc, result) => acc + result.nombre_voix, 0);
-    return {
-      candidate: candidat.nom_candidat, // Assuming 'nom' is the field for candidate name in Candidats model
-      votes: totalVotes,
-    };
-  });
+interface resultat {
+  totalVoix: number;
+  nomCandidat: string;
 }
 
 const Histogramme: React.FC = () => {
-  const { fiveDayForecast, bureauDeVote, candidatData, votesResults } =
-    useGlobalContext();
+  const { fiveDayForecast } = useGlobalContext();
   const { city, list } = fiveDayForecast;
-  const { setActiveCityCoords } = useGlobalContextUpdate();
+  const { bureauDeVote } = useGlobalContext();
+  console.log(bureauDeVote.id_bureau_vote);
 
-  if (!bureauDeVote || !candidatData || !votesResults) {
+  if (!bureauDeVote || !city || !list) {
     return <Skeleton className="h-[12rem] w-full" />;
   }
 
-  const processedCandidates = processData(votesResults, candidatData).slice(
-    0,
-    5
-  );
 
-  const totalVotes = processedCandidates.reduce(
-    (acc, candidate) => acc + candidate.votes,
-    0
-  );
+  const [fetchedVotes, setFetchedVotes] = useState<resultat[]>([]); 
+  const [totalVotes, setTotalVotes] = useState(0); 
 
+  useEffect(() => {
+  
+    const fetchVotesResults = async () => {
+      try {
+        const bureauId = bureauDeVote.id_bureau_vote;
+        console.log(bureauId)
+        const res = await axios.get(
+          `http://localhost:8080/resultats/totalVoixByCandidatAndBureauDeVote/${bureauId}`
+        );
+        console.log(res.data);
+        setFetchedVotes(res.data);
+
+        // Calculate total votes (assuming data structure from your API response)
+        const total = res.data.reduce(
+          (acc:number, result: resultat) => acc + result.totalVoix,
+          0
+        );
+        setTotalVotes(total);
+      } catch (error:any) {
+        console.log(
+          "Erreur sur la récupération des résultats: ",
+          error.message
+        );
+      }
+    };
+
+    fetchVotesResults();
+  }, [bureauDeVote]);
+
+  const hasCompleteData =
+    fetchedVotes.length > 0 &&
+    fetchedVotes.every(
+      (result) => result.nomCandidat && typeof result.totalVoix === "number"
+    );
+  
   return (
     <div className="rounded-lg border shadow-md p-12 dark:text-gray-100">
       <h2 className="font-medium mb-4 ">
         <span className="flex items-center gap-2 font-medium text-blue-500 ">
-          {pollIcon} Histogramme des Votes à {city.name}
+          {pollIcon} Histogramme des Votes à {city.name}/
+          {bureauDeVote.nom_bureau}
         </span>
       </h2>
       <div className="flex flex-col gap-4">
-        {processedCandidates.map((candidateData, i) => (
-          <div key={i} className="flex items-center gap-4 text-nowrap">
-            <div style={{ width: "120px" }}>{candidateData.candidate}:</div>
-            <div className="flex items-center w-full">
-              <div className="w-3/4 bg-gray-200 h-8 rounded-lg relative">
-                <div
-                  className="h-full rounded-lg"
-                  style={{
-                    width: `${(candidateData.votes / totalVotes) * 100}%`,
-                    backgroundColor: colors[i % colors.length],
-                  }}
-                >
-                  <span className="absolute right-2 text-xs p-2 text-gray-600">
-                    {((candidateData.votes / totalVotes) * 100).toFixed(3)}%
-                  </span>
+        {!hasCompleteData && (
+          <div>
+            Aucune donnée de vote valide n'a pu être récupérée. Veuillez
+            vérifier votre connexion internet ou contacter l'administrateur.
+          </div>
+        )}
+        {hasCompleteData && (
+          <>
+            {fetchedVotes.map((result, i) => (
+              <div key={i} className="flex items-center gap-4 text-nowrap">
+                <div style={{ width: "120px" }}>{result.nomCandidat}:</div>
+                <div className="flex items-center w-full">
+                  <div className="w-3/4 bg-gray-200 h-8 rounded-lg relative">
+                    <div
+                      className="h-full rounded-lg"
+                      style={{
+                        width: `${(result.totalVoix / totalVotes) * 100}%`,
+                        backgroundColor: colors[i % colors.length],
+                      }}
+                    >
+                      <span className="absolute right-2 text-xs p-2 text-gray-600">
+                        {((result.totalVoix / totalVotes) * 100).toFixed(3)}%
+                      </span>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="text-xl text-blue-200 ml-2 dark:text-gray-200 text-nowrap px-5">
-                {candidateData.votes} K
-              </div>
-            </div>
-          </div>
-        ))}
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
